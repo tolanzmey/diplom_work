b257518ee1a85106af0b967d7a6317d6252d3c0c
tolanzmey <61823236+tolanzmey@users.noreply.github.com>
2025-01-21 10:00:23 +0300

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
-    bucket         = "anatol-my-terraform-state-bucket"
-    key            = "anatol-dev/eks-cluster/terraform.tfstate"
+    bucket         = "anatol-terraform-state-bucket"
+    key            = "anatol/dev/eks-cluster/terraform.tfstate"
     region         = "us-east-1"
     dynamodb_table = "anatol-terraform-lock"
     encrypt        = true
